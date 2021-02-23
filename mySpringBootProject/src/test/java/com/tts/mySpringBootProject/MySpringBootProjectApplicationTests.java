239e3911cd8b4802edba2f3cd950671e9903dd4a
Hoa Dinh <hoadinh.rdms@gmail.com>
2021-02-23 04:12:13 -0600

    initial commit

---
+++ mySpringBootProject/src/test/java/com/tts/mySpringBootProject/MySpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.mySpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
